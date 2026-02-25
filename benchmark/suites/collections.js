376b2c2615e75ac9a6a0561be6891eaf9547a019
Marc Bachmann <marc.brookman@gmail.com>
2026-02-25 07:46:19 +0100

    chore: Implement empty list and empty map fast path

--- benchmark/suites/collections.js
+++ benchmark/suites/collections.js
@@ -1,5 +1,9 @@
 export default [
   {
+    name: 'Empty List Creation',
+    expression: '[]'
+  },
+  {
     name: 'List Creation',
     expression: '[1, 2, 3, 4, 5]'
   },
@@ -7,6 +11,10 @@ export default [
     name: 'Large List Creation',
     expression:
       '[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32]'
+  },
+  {
+    name: 'Empty Map Creation',
+    expression: '{}'
   },
   {
     name: 'Map Creation',
